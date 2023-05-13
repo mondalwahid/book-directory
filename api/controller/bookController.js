a17389a27d3b29c5f30223eea05706113a3facfd
mondalwahid <mondalwahid@gmail.com>
2023-05-13 14:47:20 +0530

    working on connecting to test base

--- api/controller/bookController.js
+++ api/controller/bookController.js
@@ -1,21 +1,9 @@
-// import { users } from "../Data";
+
 const books = require('../Data')
 
 const getListOfBooks = (req,res) => {
     res.json(books);
 }
-
-// const deleteBook = (req, res) => {
-//     const id = req.params.id;
-//     console.log(id)
-//     for(i = 0; i < books.length; i++){
-//         if(books[i] !== id){
-//             return true;
-//         }
-//         return false;
-//     }
-//     res.send("Book has been deleted successfully.")
-// }
 
 const createBook = (req, res) => {
     res.status(201).json({message: 'Create a book'})
--- api/server.js
+++ api/server.js
@@ -1,85 +1,81 @@
+const { PrismaClient } = require("@prisma/client");
 const express = require("express");
-// const mysql = require('mysql')
 const app = express();
-const books = require('./Data')
-app.use(express.json())
-
-
-// const connection = mysql.createConnection({
-//     host: '15.206.6.55',
-//     user: 'hourlyrooms',
-//     password: 'Hourlyr0Oms@1584',
-//     database: 'test'
-//   })
-//   connection.connect()
-//   connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-//     if (err) throw err
-//     console.log('The solution is: ', rows[0].solution)
-//   })
-//   connection.end()
-
+const prisma = new PrismaClient();
+app.use(express.json());
 
 // Api Routes
 booksRoutesRouter = require("./Routes/booksRoutes");
 
 // Create Function
-app.post('/book', function (req, res) {
-    let id = req.body.id;
-    let name = req.body.name;
-    if(id && name){
-        books.push(req.body);
-    }
-    res.status(200).send(`A new Book has been added`);
-    console.log(books)
+app.post("/book", async function (req, res) {
+  const { BookName } = req.body;
+  const createdBook = await prisma.BOOKS.create({
+    data: {
+      BookName: BookName,
+    },
   });
+  res.json(createdBook);
+});
 
 // List of books function
-app.get("/books-list", booksRoutesRouter);
+app.get("/books-list", async (req, res) => {
+  const books = await prisma.BOOKS.findMany();
+  res.json(books);
+});
 
 // book details function
-app.get(`/book/:id`, function (req, res) {
-    const id = req.params.id
-    for (let book of books) {
-        if (book.id == id) {
-            res.json(book);
-            return;
-        }
-    }
-    res.status(404).send('Book not found');
+app.get(`/book/:id`, async function (req, res) {
+  const id = parseInt(req.params.id);
+  if (id) {
+    const book = await prisma.BOOKS.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (book) {
+      res.status(200).send(book);
+    } else res.status(404).send("Not found");
+  } else res.status(403).send("forbidden");
 });
 
 // Update Function
-app.post('/book/:id', function(req, res){
-    let id = req.params.id;
-    const newBook = req.body;
-    for (let i = 0; i < books.length; i++) {
-        if (books[i].id == id) {
-            books[i] = newBook;
-        }
-    }
-    res.send('Book is edited');
+app.put("/book/:id", async function (req, res) {
+  const id = parseInt(req.params.id);
+  const newBook = req.body.BookName;
+  if(id && newBook){
+    const findId = await prisma.BOOKS.findUnique({
+        where: { id: id },
+    });
+    if(findId){
+        const updatedBook = await prisma.BOOKS.update({
+            where: { id: id },
+            data: { BookName: newBook },
+        });
+        res.status(200).send(updatedBook);   
+    } else res.status(404).send("Id not found")
+  } else res.status(403).send("Forbidden")
 });
-console.log(books);
 
 // Delete function
-app.delete(`/book/:id`, function(req, res){
-    const id = req.params.id;
-    console.log(id);
-    for(i = 0; i < books.length; i++){
-        if(books[i].id == id){
-            function findIndexes(e){
-                return e.id == books[i].id;
-            }
-            let bookIndex = books.findIndex(findIndexes);
-            books.splice(bookIndex,1);
-            res.status(200).json({message:"Book has been deleted."})
-        } else {
-            return res.status(404).json({
-                error:"No such id exists!"
-            })
-        }
-    }
-})
+app.delete(`/book/:id`, async function (req, res) {
+  const id = parseInt(req.params.id);
+  if (id) {
+    const book = await prisma.BOOKS.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (book) {
+      await prisma.BOOKS.delete({
+        where: {
+          id: id,
+        },
+      });
+      res.status(202).send("Deleted");
+    } else res.status(404).send("Not found");
+  } else res.status(403).send("forbidden");
+});
 
 app.listen(8000, () => {
   console.log("Server started on port 8000.");
